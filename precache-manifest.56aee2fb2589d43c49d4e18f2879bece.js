d358599de71ebede84ce6726e946e2401977d2c1
Micky Dore <mickydcmp@gmail.com>
2019-04-29 21:38:26 +0100

    Updates

---
+++ precache-manifest.56aee2fb2589d43c49d4e18f2879bece.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27b0ae11ab8ac6860c3909a4ad28a3c",
+    "url": "/Dobble-Practice/index.html"
+  },
+  {
+    "revision": "32f5e5ca743c6172d89f",
+    "url": "/Dobble-Practice/static/css/main.9c112015.chunk.css"
+  },
+  {
+    "revision": "eba213f648a8eee4a15b",
+    "url": "/Dobble-Practice/static/js/2.f6c98486.chunk.js"
+  },
+  {
+    "revision": "32f5e5ca743c6172d89f",
+    "url": "/Dobble-Practice/static/js/main.ea73c454.chunk.js"
+  },
+  {
+    "revision": "c937dcf5343733a51083",
+    "url": "/Dobble-Practice/static/js/runtime~main.d0dfb294.js"
+  }
+]);
